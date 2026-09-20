a383de5a8b518bcd456a25f5c25aaf97d9bbc088
youssef9449 <youssef9449@yahoo.com>
2026-09-21 02:49:03 +0300

    refactor: remove obsolete Capacitor alarm-delivery infrastructure

--- native-android/jvm-tests/src/main/java/com/capacitorjs/plugins/localnotifications/LocalNotificationsPlugin.java
+++
@@ -1,8 +0,0 @@
-package com.capacitorjs.plugins.localnotifications;
-
-import com.getcapacitor.JSObject;
-
-/** Stub for jvm-tests. */
-public class LocalNotificationsPlugin {
-    public static void fireReceived(JSObject notificationJson) {}
-}

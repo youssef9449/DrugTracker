87da6fb6da5cebc1f9832fefb09e5bda370a3944
youssef9449 <youssef9449@yahoo.com>
2026-09-21 05:10:17 +0300

    refactor: remove obsolete exact-alarm plugin imports

--- native-android/alarm-runtime/ExactAlarmPlugin.java
+++ native-android/alarm-runtime/ExactAlarmPlugin.java
@@ -1,6 +1,5 @@
 package app.drugtracker.alarmruntime;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
@@ -28,7 +27,7 @@ public final class ExactAlarmPlugin extends Plugin {
 
     @PluginMethod
     public void openSettings(PluginCall call) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.S) {
             JSObject ret = new JSObject();
             ret.put("opened", false);
             call.resolve(ret);

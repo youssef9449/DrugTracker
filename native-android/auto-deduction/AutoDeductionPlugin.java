e7c4604d6405ec988bca2cbf36841ffc2330e6e4
youssef9449 <youssef9449@yahoo.com>
2026-09-21 19:46:41 +0300

    refactor: remove unused Auto plugin import

--- native-android/auto-deduction/AutoDeductionPlugin.java
+++ native-android/auto-deduction/AutoDeductionPlugin.java
@@ -18,7 +18,6 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 

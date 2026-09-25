80c36175f7aa49ebeb41848f891012562a3e0a08
youssef9449 <youssef9449@yahoo.com>
2026-09-25 04:24:26 +0300

    test: import strict-value assertion helper

--- Tests/components/MedicationCard.manageModal.staysOpen.integration.test.tsx
+++ Tests/components/MedicationCard.manageModal.staysOpen.integration.test.tsx
@@ -1,3 +1,4 @@
+import { requireDefined } from '../helpers/requireDefined';
 /// <reference types="@testing-library/jest-dom/vitest" />
 /**
  * Management modal stays open after Take / Restore so the user can

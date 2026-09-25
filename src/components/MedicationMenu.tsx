51c2507043f276d5ada5183b7b834aa78d176486
youssef9449 <youssef9449@yahoo.com>
2026-09-25 04:15:33 +0300

    types: preserve optional callback function signatures

--- src/components/MedicationMenu.tsx
+++ src/components/MedicationMenu.tsx
@@ -19,10 +19,10 @@ export interface MedicationMenuProps {
   onEdit: (medication: Medication) => void;
   onDelete: (id: string) => void;
   onToggleAutoDeduct: (id: string) => void;
-  onToggleMedicationReminder?: (id: string) => void | undefined;
-  onToggleMedicationCriticalStockAlerts?: (id: string) => void | undefined;
-  onOpenHistory?: (medication: Medication) => void | undefined;
-  onRegisterBackHandler?: (id: string, close: () => void, priority?: number) => () => void | undefined;
+  onToggleMedicationReminder?: ((id: string) => void) | undefined;
+  onToggleMedicationCriticalStockAlerts?: ((id: string) => void) | undefined;
+  onOpenHistory?: ((medication: Medication) => void) | undefined;
+  onRegisterBackHandler?: ((id: string, close: () => void, priority?: number) => () => void) | undefined;
   size?: 'xs' | 'sm' | 'md' | undefined;
   showTypeIcon?: boolean | undefined;
   showOverflow?: boolean | undefined;

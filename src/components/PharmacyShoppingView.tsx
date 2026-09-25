e1328a33c7517aadb9334331e58a767db2d7fddc
youssef9449 <youssef9449@yahoo.com>
2026-09-25 04:15:42 +0300

    types: preserve optional callback signatures

--- src/components/PharmacyShoppingView.tsx
+++ src/components/PharmacyShoppingView.tsx
@@ -12,8 +12,8 @@ interface PharmacyShoppingViewProps {
   settings: PharmacySettings;
   onUpdateSettings: (newSettings: PharmacySettings) => void;
   showToast: (message: string) => void;
-  onOpenUserContactsSettings?: () => void | undefined;
-  onRegisterBackHandler?: (id: string, close: () => void, priority?: number) => () => void | undefined;
+  onOpenUserContactsSettings?: (() => void) | undefined;
+  onRegisterBackHandler?: ((id: string, close: () => void, priority?: number) => () => void) | undefined;
 }
 
 export const PharmacyShoppingView: FC<PharmacyShoppingViewProps> = ({

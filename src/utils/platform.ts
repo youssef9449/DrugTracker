6f9ea29ef183e49af4da50314a01474a353117fa
youssef9449 <youssef9449@yahoo.com>
2026-09-26 14:35:24 +0300

    chore: remove accidental platform helper from main

--- src/utils/platform.ts
+++
@@ -1,9 +0,0 @@
-import { Capacitor } from '@capacitor/core';
-
-export function isAndroidPlatform(): boolean {
-  try {
-    return typeof Capacitor !== 'undefined' && Capacitor.getPlatform() === 'android';
-  } catch {
-    return false;
-  }
-}
